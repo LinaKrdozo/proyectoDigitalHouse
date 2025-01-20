69d862df1e72ba1d2e3fc7c888d3fc0119699147
Lina <usuariomac@MacBook-Pro-de-Usuario.local>
2025-01-20 12:12:29 -0500

    cambio en password bd

--- src/model/db/config/database.js
+++ src/model/db/config/database.js
@@ -1,25 +1,25 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "password",
+    "password": "root12345",
     "database": "digitalBees",
-    "host": "localhost",
+    "host": "127.0.0.1",
     "port": 3306,
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": "password",
+    "password": "root12345",
     "database": "digitalBees",
-    "host": "localhost",
+    "host": "127.0.0.1",
     "port": 3306,
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": "password",
+    "password": "root12345",
     "database": "digitalBees",
-    "host": "localhost",
+    "host": "127.0.0.1",
     "port": 3306,
     "dialect": "mysql"
   }
